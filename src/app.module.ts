bef347c2d70f44df01b24eef641829dbca42cead
Rodrigo Ramalho <hodrigohamalho@gmail.com>
2019-08-14 21:17:49 -0300

    enabling CORS

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
-import { Module, HttpModule } from '@nestjs/common';
+import { Module, HttpModule, MiddlewareConsumer, RequestMethod } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import * as cors from "cors";
 
 @Module({
   imports: [HttpModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(cors())
+      .forRoutes({ path: '*', method: RequestMethod.ALL });
+  }
+}
