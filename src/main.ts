69c4014ac51597d7ae1ab43d60e2518576f7a011
Rodrigo Ramalho <hodrigohamalho@gmail.com>
2019-08-18 22:14:39 -0300

    port as environment variable

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(4000);
+
+  let port = process.env.SERVER_PORT || 4000;
+  await app.listen(port);
 }
 bootstrap();
